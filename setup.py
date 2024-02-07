77ecd7283b8882052c4fec8480c3ab56ac83c0e5
coralexbadea <coralexbadea99@gmail.com>
2024-02-07 11:49:00 +0200

    update v1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyWiseTools",
-    version="0.1.0",  # Update with your package version
+    version="0.1.2",  # Update with your package version
     description="A description of your package",
     author="Your Name",
     author_email="your.email@example.com",
     url="https://github.com/coralexbadea/pyWiseTools",
     packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pytesseract",
+        "pyautogui",
+        "opencv-python"
+        # Add other dependencies here
+    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
